69a54d116da2bf70049afe7e4a57aa8fcec505aa
Rosen Stanchev <rosenstanchev@Rosens-MacBook-Pro.local>
2024-03-18 01:10:55 +0100

    Changing Axios address to Cloud Run

--- frontend/src/api/FoodProductsAPI.js
+++ frontend/src/api/FoodProductsAPI.js
@@ -3,41 +3,128 @@ import axios from 'axios';
 // const getToken=()=>{
 //   return localStorage.getItem('USER_KEY');
 // }
+
+// LocalHost -----------------------------------------------
+// export  const fetchFoodProducts=()=>{
+//     return axios({
+//         method:'GET',
+//         url:`${process.env.hostUrl||'http://localhost:8080'}/api/food-product`,
+//         headers:{
+//       }
+//     })
+// }
+// export const deleteFoodProduct = (id) => {
+//   return axios({
+//     method: 'DELETE',
+//     url: `${process.env.hostUrl || 'http://localhost:8080'}/api/food-product/${id}`,
+//     headers: {
+//       // Add any headers if needed
+//     }
+//   });
+// };
+
+// export const updateFoodProduct = (updatedProduct) => {
+//   return axios({
+//     method: 'PUT',
+//     url: `${process.env.hostUrl || 'http://localhost:8080'}/api/food-product/${updatedProduct.id}`,
+//     data: updatedProduct,
+//     headers: {
+//       // Add headers if needed
+//     }
+//   });
+// };
+// export const createFoodProduct = (updatedProduct) => {
+//   return axios({
+//     method: 'POST',
+//     url: `${process.env.hostUrl || 'http://localhost:8080'}/api/food-product`,
+//     data: updatedProduct,
+//     headers: {
+//       // Add headers if needed
+//     }
+//   });
+// };
+
+
+
+
+// API-Gateway -----------------------------------------------
+// export  const fetchFoodProducts=()=>{
+//     return axios({
+//         method:'GET',
+//         url:`${process.env.hostUrl||'http://localhost:8080'}/api/food-product`,
+//         headers:{
+//       }
+//     })
+// }
+// export const deleteFoodProduct = (id) => {
+//   return axios({
+//     method: 'DELETE',
+//     url: `${process.env.hostUrl || 'http://localhost:8080'}/api/food-product/${id}`,
+//     headers: {
+//       // Add any headers if needed
+//     }
+//   });
+// };
+
+// export const updateFoodProduct = (updatedProduct) => {
+//   return axios({
+//     method: 'PUT',
+//     url: `${process.env.hostUrl || 'http://localhost:8080'}/api/food-product/${updatedProduct.id}`,
+//     data: updatedProduct,
+//     headers: {
+//       // Add headers if needed
+//     }
+//   });
+// };
+// export const createFoodProduct = (updatedProduct) => {
+//   return axios({
+//     method: 'POST',
+//     url: `${process.env.hostUrl || 'http://localhost:8080'}/api/food-product`,
+//     data: updatedProduct,
+//     headers: {
+//       // Add headers if needed
+//     }
+//   });
+// };
+
+
+
+// Cloud-Run -----------------------------------------------
 export  const fetchFoodProducts=()=>{
-    return axios({
-        method:'GET',
-        url:`${process.env.hostUrl||'http://localhost:8080'}/api/food-product`,
-        headers:{
-      }
-    })
+  return axios({
+      method:'GET',
+      url:`${process.env.hostUrl||'https://foodmotion-food-products-service-ud3f4nfe5a-ew.a.run.app'}/api/food-product`,
+      headers:{
+    }
+  })
 }
 export const deleteFoodProduct = (id) => {
-  return axios({
-    method: 'DELETE',
-    url: `${process.env.hostUrl || 'http://localhost:8080'}/api/food-product/${id}`,
-    headers: {
-      // Add any headers if needed
-    }
-  });
+return axios({
+  method: 'DELETE',
+  url: `${process.env.hostUrl || 'https://foodmotion-food-products-service-ud3f4nfe5a-ew.a.run.app'}/api/food-product/${id}`,
+  headers: {
+    // Add any headers if needed
+  }
+});
 };
 
 export const updateFoodProduct = (updatedProduct) => {
-  return axios({
-    method: 'PUT',
-    url: `${process.env.hostUrl || 'http://localhost:8080'}/api/food-product/${updatedProduct.id}`,
-    data: updatedProduct,
-    headers: {
-      // Add headers if needed
-    }
-  });
+return axios({
+  method: 'PUT',
+  url: `${process.env.hostUrl || 'https://foodmotion-food-products-service-ud3f4nfe5a-ew.a.run.app'}/api/food-product/${updatedProduct.id}`,
+  data: updatedProduct,
+  headers: {
+    // Add headers if needed
+  }
+});
 };
 export const createFoodProduct = (updatedProduct) => {
-  return axios({
-    method: 'POST',
-    url: `${process.env.hostUrl || 'http://localhost:8080'}/api/food-product`,
-    data: updatedProduct,
-    headers: {
-      // Add headers if needed
-    }
-  });
+return axios({
+  method: 'POST',
+  url: `${process.env.hostUrl || 'https://foodmotion-food-products-service-ud3f4nfe5a-ew.a.run.app'}/api/food-product`,
+  data: updatedProduct,
+  headers: {
+    // Add headers if needed
+  }
+});
 };
